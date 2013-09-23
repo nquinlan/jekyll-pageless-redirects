434c3094c2e78ab4c05e41ede98371dfbd4a89c2
Jay Anslow <jay@anslow.me.uk>
2013-09-23 01:05:54 +0100

    Fixed incorrect source link (used to 404)

--- pageless_redirects.rb
+++ pageless_redirects.rb
@@ -45,7 +45,7 @@
 #
 # Author: Nick Quinlan
 # Site: http://nicholasquinlan.com
-# Plugin Source: http://github.com/nquinlan/jekyll-pageless-redirect
+# Plugin Source: https://github.com/nquinlan/jekyll-pageless-redirects
 # Plugin License: MIT
 # Plugin Credit: This plugin borrows heavily from alias_generator (http://github.com/tsmango/jekyll_alias_generator) by Thomas Mango (http://thomasmango.com)
 
