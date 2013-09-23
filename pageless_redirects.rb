aeb9cbd241ca7138cbba2fbb957cafe1d384980e
Jay Anslow <jay@anslow.me.uk>
2013-09-23 01:10:53 +0100

    Made alias template W3C compliant.
    Added space after semicolon in refresh meta tag
    Added page title

--- pageless_redirects.rb
+++ pageless_redirects.rb
@@ -130,9 +130,10 @@ module Jekyll
       <!DOCTYPE html>
       <html>
       <head>
+      <title>Redirecting...</title>
       <link rel="canonical" href="#{destination_path}"/>
       <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta http-equiv="refresh" content="0;url=#{destination_path}" />
+      <meta http-equiv="refresh" content="0; url=#{destination_path}" />
       </head>
       </html>
       EOF
